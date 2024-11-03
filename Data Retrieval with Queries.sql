715b10f63fb942e8a688e0a97ab087986d8e9d09
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-03 15:27:22 +0530

    Update

---
+++ Data Retrieval with Queries.sql
@@ -0,0 +1,15 @@
+-- Select specific columns
+SELECT first_name, last_name, salary
+FROM employees;
+
+-- Select with filtering and sorting
+SELECT first_name, last_name, salary
+FROM employees
+WHERE department_id = 101
+ORDER BY salary DESC;
+
+-- Select with aggregate functions
+SELECT department_id, AVG(salary) AS average_salary
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) > 50000;
