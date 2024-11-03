3f8a5d17d5d7c0801c1f560b31f77805515c6e46
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-03 15:28:55 +0530

    Update

---
+++ Views.sql
@@ -0,0 +1,11 @@
+-- Create a view
+CREATE VIEW employee_salaries AS
+SELECT first_name, last_name, salary
+FROM employees
+WHERE salary > 50000;
+
+-- Query a view
+SELECT * FROM employee_salaries;
+
+-- Drop a view
+DROP VIEW employee_salaries;
