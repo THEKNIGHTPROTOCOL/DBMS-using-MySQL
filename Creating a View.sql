58a48cf27b6998ae42d9e52c2053ba4e59a598ed
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-28 12:31:20 +0530

    Update

---
+++ Creating a View.sql
@@ -0,0 +1,8 @@
+-- Create a view for employees with salary > 70000
+CREATE VIEW HighSalaryEmployees AS
+SELECT EmployeeID, FirstName, LastName, Salary
+FROM Employees
+WHERE Salary > 70000;
+
+-- Query the view
+SELECT * FROM HighSalaryEmployees;
