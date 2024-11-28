503cd5b23ad81ba436802dad575f331600a3e3f3
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-28 12:18:23 +0530

    Update

---
+++ Joining Tables.sql
@@ -0,0 +1,10 @@
+-- Join Employees and Departments
+SELECT 
+    e.EmployeeID, 
+    e.FirstName, 
+    e.LastName, 
+    d.DepartmentName, 
+    e.Salary
+FROM Employees e
+JOIN Departments d
+ON e.DepartmentID = d.DepartmentID;
