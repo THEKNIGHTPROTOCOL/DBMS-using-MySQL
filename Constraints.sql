32513dc13e3984e2b5d77070e95474e097d40eef
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-02 21:37:39 +0530

    Update Constraints.sql

--- Constraints.sql
+++ Constraints.sql
@@ -5,7 +5,7 @@ MODIFY salary DECIMAL(10, 2) NOT NULL;
 -- Add a UNIQUE constraint                               
 ALTER TABLE employees                
 ADD CONSTRAINT unique_email UNIQUE (email);         
-
+                 
 -- Add a PRIMARY KEY constraint       
 ALTER TABLE employees   
 ADD PRIMARY KEY (employee_id); 
