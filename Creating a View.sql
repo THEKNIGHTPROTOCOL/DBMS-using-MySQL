cf5f515170442ea6d16a6b1d809f42bda0c4d16e
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-02 21:44:50 +0530

    Update Creating a View.sql

--- Creating a View.sql
+++ Creating a View.sql
@@ -2,7 +2,7 @@
 CREATE VIEW HighSalaryEmployees AS
 SELECT EmployeeID, FirstName, LastName, Salary
 FROM Employees
-WHERE Salary > 70000;
+WHERE Salary > 70000;                                                             
 
 -- Query the view
 SELECT * FROM HighSalaryEmployees;
