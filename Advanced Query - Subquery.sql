61807645dfdc6f82160bb20ec496bbc380289b3a
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-02 20:57:45 +0530

    Update Advanced Query - Subquery.sql

--- Advanced Query - Subquery.sql
+++ Advanced Query - Subquery.sql
@@ -2,7 +2,8 @@
 SELECT MAX(Salary) AS SecondHighestSalary
 FROM Employees
 WHERE Salary < (SELECT MAX(Salary) FROM Employees);
-                 
+                            
                      
          
      
+ 
